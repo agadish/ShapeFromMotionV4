51e6bd55204c6a6d5139bab8b5996143ff229765
Assaf <assaf@omnom.nom>
2022-04-29 15:38:15 +0300

    Seems ready for experiment

--- sfm_experiment_main.m
+++ sfm_experiment_main.m
@@ -1,17 +1,21 @@
 close all;
 clear all;
 clc;
+
+Date = datestr(now,30);
 %Please enter your name
 Name = 'Assaf';
 
+% We will perform experiment type 1 twice, and type 2 twice
+% When you're ready please press F5           
 
-Date = datestr(now,30);
-
-% We will perform experiment type 2 twice, and type 3 twice
 Exp_Type = 1;
-
-%When you're ready please press F5
-sfm_experiment3(Name,Date,Exp_Type);
-
- 
-%Good Luck 
+sfm_experiment3(Name ,Date,Exp_Type); 
+Exp_Type = 2;
+sfm_experiment3(Name ,Date,Exp_Type); 
+Exp_Type = 1;
+sfm_experiment3(Name ,Date,Exp_Type); 
+Exp_Type = 2;       
+sfm_experiment3(Name ,Date,Exp_Type); 
+         
+% Good Luck
