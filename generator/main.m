5a1b0e74c3a55f5ea3b0e188899f8dd4527c0b81
Assaf <assaf@omnom.nom>
2022-04-29 12:26:23 +0300

    Updated FPS and files names

--- generator/main.m
+++ generator/main.m
@@ -14,7 +14,7 @@ function main(varargin)
         obj1_mean = obj_mean(1);
         obj2_mean = obj_mean(2);
         for obj_sd = [0.1, 0.7, 1.5]
-            % in case main was called without arguments      
+            % in case main was called without arguments
             config_file_name = Configuration(rand_flag,IsObjVideo,debug_mode, obj1_mean, obj_sd, obj2_mean, obj_sd);
             load(config_file_name);
 
